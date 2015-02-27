6f1b3bf630a3a760c8551d9cb07f8ac7204de01b
Pedro Ventura <pedroventurasilva@gmail.com>
2015-02-27 22:05:02 +0000

    javascript intro

---
+++ javascript-pedro/script.js
@@ -0,0 +1,66 @@
+// Comentários
+/*
+	alert("Hello from script.js!");
+*/
+// Single - line Comment
+
+// Mensagens na consola
+console.log("Hello from console!");
+console.warn("This is a warning.");
+console.error("This is an error.");
+
+// Declaração de variáveis em Javascript
+var aMinhaVar;
+var aMinhaOutraVar = 5;
+aMinhaVar = 10;
+var name = 'Bob';
+var boolVar = true;
+var arrayVar = ['Bob', 'Joe', 'Sam'];
+
+console.log(aMinhaVar);
+console.log(aMinhaOutraVar);
+console.log(typeof(aMinhaVar));
+console.log(typeof(name));
+console.log(typeof(boolVar));
+console.log(typeof(arrayVar));
+
+// Operadores Aritméticos
+	
+// Adição
+console.log("\nAdição");
+console.log(2+2);
+console.log(aMinhaVar + 2);
+console.log(aMinhaVar + aMinhaOutraVar);
+
+// Subtração
+console.log("\nSubtração");
+console.log(2-2);
+console.log(aMinhaVar - 2);
+console.log(aMinhaVar - aMinhaOutraVar);
+
+// Multiplicação
+console.log("\nMultiplicação");
+console.log(2*2);
+console.log(aMinhaVar * 2);
+console.log(aMinhaVar * aMinhaOutraVar);
+
+// Divisão
+console.log("\nDivisão");
+console.log(2/2);
+console.log(aMinhaVar / 2);
+console.log(aMinhaVar / aMinhaOutraVar);
+
+// Módulo
+console.log("\nMódulo");
+console.log(5%2);
+console.log(5/2);
+
+// Incremento
+aMinhaVar++;
+console.log("\nIncremento");
+console.log(aMinhaVar);
+
+// Decremento
+aMinhaOutraVar--;
+console.log("\nDecremento");
+console.log(aMinhaOutraVar);
