471c8f3073a1771b1dab2ec69f9db13365160f9d
anaidgit <dianamonteiro92@gmail.com>
2015-03-02 19:17:00 +0000

    no message

---
+++ javascript_Diana/js/script.js
@@ -0,0 +1,69 @@
+/*
+alert('Hello world');
+*/
+
+
+//Mensagens na consola
+console.log("Hello world");
+
+
+//declaração de variáveis
+var aMinhaVar;
+
+var aMinhaOutraVar = 5;
+
+var aMinhaVar=10;
+
+var name = 'Bob';
+
+var boolvar = true;
+
+var arrayVar = ['Bob', 'Joe', 'Sam'];
+
+console.log(aMinhaVar);
+console.log(name);
+console.log(typeof(aMinhaVar));
+console.log(typeof(name));
+console.log(typeof(boolvar));
+console.log(typeof(arrayVar));
+
+
+//operadores aritméticos
+
+	//Adição
+	console.log("\nAdição");
+	console.log(2+2);
+	console.log(aMinhaVar + 2);
+	console.log(aMinhaVar + aMinhaOutraVar);
+
+	//Subtração
+	console.log("\nSubtração");
+	console.log(2-2);
+	console.log(aMinhaVar - 2);
+	console.log(aMinhaVar - aMinhaOutraVar);
+	
+	//Multiplicando
+	console.log("\nMultiplicação");
+	console.log(2*2);
+	console.log(aMinhaVar * 2);
+	console.log(aMinhaVar * aMinhaOutraVar);
+
+	//Divisão
+	console.log("\nDivisão");
+	console.log(2/2);
+	console.log(aMinhaVar / 2);
+	console.log(aMinhaVar / aMinhaOutraVar);
+
+
+	//Módulo
+	console.log("\nMódulo");
+	console.log(5%2);
+	console.log(aMinhaVar % 2);
+	console.log(aMinhaVar % aMinhaOutraVar);
+
+
+	//Incremento
+	aMinhaVar++;
+	console.log(aMinhaVar);
+
+	//Decremento
