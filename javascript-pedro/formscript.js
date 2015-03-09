211edc25af487160cee31748755e61e468324649
Pedro Ventura <pedroventurasilva@gmail.com>
2015-03-09 21:57:05 +0000

    validacao form parte 1

--- javascript-pedro/formscript.js
+++ javascript-pedro/formscript.js
@@ -1,36 +1,43 @@
 var btnHello = document.getElementById("btnHello");
-console.log(btnHello);
-console.log(typeof btnHello);
+var btnSend = document.getElementById("btnSend");
+var myForm = document.getElementsByTagName("form")[0];
+var mySpan = document.getElementsByTagName("span")[0];
+
+var myLabels = myForm.getElementsByTagName("label");
+var myFields = [myForm.name, myForm.email, myForm.message];
+
 btnHello.addEventListener("click", openForm);
+btnHello.addEventListener("mouseover", changeSpan);
+btnHello.addEventListener("mouseout", changeSpan);
+
+btnSend.addEventListener("click", validateForm);
+
+function validateForm(evt){
+	var validForm = true;
+	// Testar se campos estão vazios
+	for(var i = 0;  i < myFields.length; i++){		
+		if (myFields[i].value == ""){
+			myLabels[i].textContent += " Erro!";
+			validForm = false;
+		}
+	}
+	// if (!validForm) é o mesmo que if(validForm == false)
+	if (!validForm) {
+		evt.preventDefault();
+		console.log('Form Inválido');
+	}  	
+}
 function openForm(){
-	var myForm = document.getElementsByTagName("form")[0];
 	if (myForm.style.display == "" || myForm.style.display == "none") {
 		myForm.style.display = "block";
 	}else{
 		myForm.style.display = "none";
 	}
 }
-
-btnHello.addEventListener("mouseover", changeSpan);
-btnHello.addEventListener("mouseout", changeSpan);
 function changeSpan(oMeuEvento){
 	if(oMeuEvento.type=="mouseover"){
-		document.getElementsByTagName("span")[0].style.opacity=1;
+		mySpan.style.opacity=1;
 	}else{
-		document.getElementsByTagName("span")[0].style.opacity=0;
+		mySpan.style.opacity=0;
 	}
 }
-
-
-/*
-btnHello.addEventListener("mouseover", showSpan);
-btnHello.addEventListener("mouseout", hideSpan);
-function showSpan(){
-	var mySpan = document.getElementsByTagName("span")[0];
-		mySpan.style.display = "block";
-}
-function hideSpan(){
-	var mySpan = document.getElementsByTagName("span")[0];
-		mySpan.style.display = "none";
-}
-*/
