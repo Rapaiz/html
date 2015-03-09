549b9e562e325320533e08147cd5b73bac71debb
Rafael Tomé <rafaeltome@hotmail.com>
2015-03-09 21:55:14 +0000

    alterações formulário JS

--- html_rafael/form/formscript.js
+++ html_rafael/form/formscript.js
@@ -1,9 +1,26 @@
 var btnHello = document.getElementById("btnHello");
-
+var btnSend = document.getElementById("btnSend");
+var form = document.getElementsByTagName("form")[0];
+var span = document.getElementsByTagName("span")[0];
+var myLabels = form.getElementsByTagName("label");
+var myFields = [form.name, form.email, form.message];
 btnHello.addEventListener("click", openForm);
+btnHello.addEventListener("mouseover", changeSpan);
+btnHello.addEventListener("mouseout", changeSpan);
+
+btnSend.addEventListener("click", validateForm);
+function validateForm(evt){
+	var validForm = true;
+	for (var i = 0; i < myFields.length; i++) {
+		if (myFields[i].value == ""){
+		myLabels[i].textContent += " Erro!";
+		validForm = false;
+		}
+	}
+	if (!validForm) {evt.preventDefault();}
+}
 
 function openForm(){
-	var form = document.getElementsByTagName("form")[0];
 	if (form.style.display == "" || form.style.display == "none") {
 		form.style.display = "block";
 	}else{
@@ -11,14 +28,10 @@ function openForm(){
 	}
 }
 
-btnHello.addEventListener("mouseover", changeSpan);
-btnHello.addEventListener("mouseout", changeSpan);
 function changeSpan(oMeuEvento){
 	if(oMeuEvento.type=="mouseover"){
-		document.getElementsByTagName("span")[0].style.opacity=1;
+		span.style.opacity=1;
 	}else{
-		document.getElementsByTagName("span")[0].style.opacity=0;
+		span.style.opacity=0;
 	}
 }
-
-/**/
