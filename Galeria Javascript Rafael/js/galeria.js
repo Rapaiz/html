e4e3573803bfb412eb8d9036207b0e7be9d7013b
Rafael Tomé <rafaeltome@hotmail.com>
2015-03-13 22:05:42 +0000

    galeria js

---
+++ Galeria Javascript Rafael/js/galeria.js
@@ -0,0 +1,35 @@
+var imagemGrande = document.getElementById("imagemGrande");
+var imagensPequenas = document.getElementById("imagens").getElementsByTagName("img");
+var btnLeft = document.getElementById("btn_left");
+var btnRight = document.getElementById("btn_right");
+var imagensDiv = document.getElementById("imagens");
+
+
+
+btnLeft.addEventListener("click", moverImagens);
+btnRight.addEventListener("click", moverImagens);
+imagensDiv.style.right = "0px"
+
+function moverImagens(){
+	if (this.id == "btn_left"){
+		if (parseInt(imagensDiv.style.right)<360) {
+			imagensDiv.style.right = parseInt(imagensDiv.style.right) + 20 + "px";
+		}
+	}else{
+		if (parseInt(imagensDiv.style.right)>0) {
+			imagensDiv.style.right = parseInt(imagensDiv.style.right) - 20 + "px";
+		}
+	}
+}
+
+
+for (var i = 0; i < imagensPequenas.length; i++) {
+	imagensPequenas[i].addEventListener("click", trocarImagem)
+}
+
+function trocarImagem(){
+	imagemGrande.src = "img/santorini" + this.id.substring(4) + ".jpg";
+}
+
+
+// = "img/santorini2.jpg";
